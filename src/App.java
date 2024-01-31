8365bae03dbea17b37d8fe22ad9424c21966c60a
unknown <mononyane121@gmail.com>
2024-01-31 14:11:25 +0200

    My first commit

---
+++ src/App.java
@@ -0,0 +1,13 @@
+
+public class App {
+
+    static void Addition(){
+        int num1 = 1, num2 = 1;
+        int results = num1 + num2;
+        System.out.println(results);
+    }
+    public static void main(String[] args) throws Exception {
+        Addition();    
+    }
+}
+
